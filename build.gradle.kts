25d4af8e1d0c2f0aeebc547c8302547d0b64000e
Florian Roks <froks@users.noreply.github.com>
2025-08-03 21:47:45 +0200

    update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    val kotlinVersion = "2.1.21"
+    val kotlinVersion = "2.2.0"
     kotlin("jvm") version kotlinVersion
     id("com.github.johnrengelman.shadow") version "8.1.0"
     application
@@ -17,15 +17,13 @@ dependencies {
     implementation(kotlin("stdlib"))
 
     // You should use the latest released stable version
-    implementation("io.github.doip-sim-ecu:doip-sim-ecu-dsl:0.20.1-beta")
+    implementation("io.github.doip-sim-ecu:doip-sim-ecu-dsl:0.21.0")
 
     implementation("ch.qos.logback:logback-classic:1.5.18") // EPL-1.0
 }
 
-tasks {
-    application {
-        mainClass.set("MainKt")
-    }
+application {
+    mainClass.set("MainKt")
 }
 
 kotlin {
